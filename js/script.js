6a4d13df33db3216359e7bd83b556449822b3d32
mfps <f.s.wille@gmail.com>
2017-10-13 21:57:35 +0200

    adding functionality

--- js/script.js
+++ js/script.js
@@ -1,2 +1,158 @@
 import './fetch';
 import './promise.min';
+
+import WeatherServices from './services';
+
+class WeatherApp {
+    constructor() {
+        this.state = {};
+        this.category = 'all';
+        this.foreCastButton = document.querySelector('.forecast-button');
+        this.location = document.querySelector('#location');
+        this.resultOptions = document.querySelectorAll('.options div');
+        this.conditions = document.querySelector('.conditions');
+        this.weatherServices = new WeatherServices();
+        this.onInit();
+    }
+
+    eventHandler() {
+        this.foreCastButton.addEventListener('click', e => {
+            e.preventDefault();
+            this.getWeatherData()
+        }, false)
+
+        this.resultOptions.forEach(el => {
+            el.addEventListener('click', e => {
+                e.preventDefault();
+                this.updateActivityList(e)
+            }, false);
+        });
+    }
+
+    getWeatherData() {
+        const location = this.location.value;
+        this.location.value = '';
+
+        this.weatherServices
+            .getWeather(location)
+            .then(response => {
+                this.updateUISuccess(response);
+            }).catch(error => {
+                this.updateUIFailure(error);
+            });
+    }
+
+    updateUISuccess(response) {
+        const degC = response.main.temp - 273.15;
+        const degCInt = Math.floor(degC);
+        const degF = degC * 1.8 + 32;
+        const degFInt = Math.floor(degF);
+        this.state = {
+            condition: response.weather[0].main,
+            icon: `http://openweathermap.org/img/w/${response.weather[0].icon}.png`,
+            degCInt: Math.floor(degCInt),
+            degFInt: Math.floor(degFInt),
+            city: response.name
+        };
+
+        const into = this.conditions;
+
+        let container = document.createElement('div');
+        let cityPara = document.createElement('p');
+        cityPara.setAttribute('class', 'city');
+        cityPara.textContent = this.state.city;
+        let conditionsPara = document.createElement('p');
+        conditionsPara.textContent = `${this.state.degCInt}\u00B0 C / ${this.state.degFInt}\u00B0 F`;
+        let iconImage = document.createElement('img');
+        iconImage.setAttribute('src', this.state.icon);
+        iconImage.setAttribute('alt', this.state.condition);
+        conditionsPara.appendChild(iconImage);
+        container.appendChild(cityPara);
+        container.appendChild(conditionsPara);
+
+        const conditions = document.querySelector('.conditions div');
+
+        if (conditions) {
+            into.replaceChild(container, conditions);
+        } else {
+            into.appendChild(container);
+        }
+
+        this.updateActivityList();
+    }
+
+    updateUIFailure(error) {
+        document.querySelector(".conditions").textContent = "Weather information unavailable";
+    }
+
+    async updateActivityList(event) {
+        console.log(event);
+        if (event !== undefined && event.target.classList.contains('selected')) {
+            return true;
+
+        } else if (event !== undefined && !event.target.classList.contains('selected')) {
+            this.category = event.target.id;
+
+            this.resultOptions.forEach(function (el) {
+                el.classList.remove('selected');
+            });
+
+            event.target.classList.add('selected');
+        }
+
+        this.state.activities = [];
+        if (this.state.condition === "Rain") {
+            await this.updateState('In');
+        } else if (this.state.condition === "Snow" || this.state.degFInt < 50) {
+            await this.updateState('OutCold');
+        } else {
+            await this.updateState('OutWarm');
+        }
+
+        const into = document.querySelector('.activities');
+        const activities = document.querySelector('.activities div');
+        let activitiesContainer = document.createElement('div');
+        let list = document.createElement('ul');
+
+        this.state.activities.forEach(function (activity, index) {
+            let listItem = document.createElement('li');
+            listItem.setAttribute('key', index);
+            listItem.textContent = activity;
+            list.appendChild(listItem);
+        });
+        activitiesContainer.appendChild(list);
+
+        if (activities) {
+            into.replaceChild(activitiesContainer, activities);
+        } else {
+            into.appendChild(activitiesContainer);
+        }
+
+        document.querySelector('.results').classList.add('open');
+    }
+
+    async updateState(type) {
+
+        const activities = await this.weatherServices
+            .getActivities()
+            .then(activities => activities);
+
+        if (this.category === "solo") {
+            this.state.activities.push(...activities['solo' + type]);
+        } else if (this.category === "team") {
+            this.state.activities.push(...activities['team' + type]);
+        } else {
+            this.state.activities.push(...activities['solo' + type]);
+            this.state.activities.push(...activities['team' + type]);
+        }
+    }
+
+    onInit() {
+        this.eventHandler();
+    }
+}
+
+(() => {
+
+    new WeatherApp();
+})();
---
+++ js/services.js
@@ -0,0 +1,25 @@
+export default class WeatherServices {
+    constructor() { }
+
+    getActivities() {
+        return new Promise(resolve => {
+            return resolve({
+                teamIn: ['basketball', 'hockey', 'volleyball'],
+                teamOutWarm: ['softball/baseball', 'football/soccer', 'American football', 'rowing', 'tennis', 'volleyball', 'ultimate frisbee', 'rugby'],
+                teamOutCold: ['hockey'],
+                soloIn: ['rock climbing', 'swimming', 'ice skating'],
+                soloOutWarm: ['rowing', 'running', 'hiking', 'cycling', 'rock climbing'],
+                soloOutCold: ['snowshoeing', 'downhill skiing', 'cross-country skiing', 'ice skating']
+            });
+        });
+    }
+
+    getWeather(location) {
+        const URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+        const API_KEY = '';
+
+        return fetch(`${URL}${location}&appid=${API_KEY}`).then(response => {
+            return response.json();
+        });
+    }
+}
